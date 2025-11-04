ed52e3dc2acc83783f2e22ef0534578ada24c429
manik <maniksarker652@gmail.com>
2025-11-04 12:52:19 +0600

    s

--- src/app/constant/index.image.ts
+++ src/app/constant/index.image.ts
@@ -1,60 +1,60 @@
-import happyBanner from "@/../public/assets/image/happy.png"
-import footersImage from "@/../public/assets/image/Footers.svg"
-import brandV2 from "@/../public/assets/image/brandV2.png"
-import bannerImage from "@/../public/assets/image/banenrImage.webp"
-import sockerKid from "@/../public/assets/image/socker-kid.webp"
-import join from "@/../public/assets/image/join.png"
-import slideImage1 from "@/../public/assets/image/slide-1.webp"
-import slideImage2 from "@/../public/assets/image/slide-2.png"
-import slideImage3 from "@/../public/assets/image/slide-3.png"
-import discoverBannerImage from "@/../public/assets/image/layer-discover.png"
-import aboutUsImage from "@/../public/assets/image/about-us.png"
-import contactImage from "@/../public/assets/image/conatct.png"
-import ceo from "@/../public/assets/image/ceo.png"
-import vice from "@/../public/assets/image/vice.png"
-import whoWeAre from "@/../public/assets/image/Who We Are.png"
-import ourStory from "@/../public/assets/image/Our Story.png"
-import whatDrivesUs from "@/../public/assets/image/What Drives Us.png"
-import howWeIgniteChange from "@/../public/assets/image/How We Ignite Change.png"
-import SECRETARY from "@/../public/assets/image/SECRETARY.png"
-import students from "@/../public/assets/image/sports-teacher-with-her-students.jpg"
-
+import aboutUsImage from "@/../public/assets/image/about-us.png";
+import bannerImage from "@/../public/assets/image/banenrImage.webp";
+import brandV2 from "@/../public/assets/image/brandV2.png";
+import ceo from "@/../public/assets/image/ceo.png";
+import contactImage from "@/../public/assets/image/conatct.png";
+import footersImage from "@/../public/assets/image/Footers.svg";
+import happyBanner from "@/../public/assets/image/happy.png";
+import howWeIgniteChange from "@/../public/assets/image/How We Ignite Change.png";
+import join from "@/../public/assets/image/join.png";
+import discoverBannerImage from "@/../public/assets/image/layer-discover.png";
+import ourStory from "@/../public/assets/image/Our Story.png";
+import SECRETARY from "@/../public/assets/image/SECRETARY.png";
+import slideImage1 from "@/../public/assets/image/slide-1.webp";
+import slideImage2 from "@/../public/assets/image/slide-2.png";
+import slideImage3 from "@/../public/assets/image/slide-3.png";
+import sockerKid from "@/../public/assets/image/socker-kid.webp";
+import students from "@/../public/assets/image/sports-teacher-with-her-students.jpg";
+import vice from "@/../public/assets/image/vice.png";
+import whatDrivesUs from "@/../public/assets/image/What Drives Us.png";
+import whoWeAre from "@/../public/assets/image/Who We Are.png";
+// make some chagnes
 export const IMAGE = {
-    happyBanner,
-    footersImage,
-    brandV2,
-    bannerImage,
-    sockerKid,
-    join,
-    slideImage1,
-    slideImage2,
-    slideImage3,
-    discoverBannerImage,
-    aboutUsImage,
-    contactImage,
-    ceo,
-    vice,
-    SECRETARY,
-    whoWeAre,
-    ourStory,
-    whatDrivesUs,
-    howWeIgniteChange,
-    students
-}
+  happyBanner,
+  footersImage,
+  brandV2,
+  bannerImage,
+  sockerKid,
+  join,
+  slideImage1,
+  slideImage2,
+  slideImage3,
+  discoverBannerImage,
+  aboutUsImage,
+  contactImage,
+  ceo,
+  vice,
+  SECRETARY,
+  whoWeAre,
+  ourStory,
+  whatDrivesUs,
+  howWeIgniteChange,
+  students,
+};
 
-import fire from "@/../public/assets/icons/fire.svg"
-import archer from "@/../public/assets/icons/archar.svg"
-import idea from "@/../public/assets/icons/idea.svg"
-import magnifire from "@/../public/assets/icons/magnifire.svg"
-import stadium from "@/../public/assets/icons/stadium.svg"
-import roket from "@/../public/assets/icons/roket.svg"
-import imageFrame from "@/../public/assets/icons/ImageFrame.png"
+import archer from "@/../public/assets/icons/archar.svg";
+import fire from "@/../public/assets/icons/fire.svg";
+import idea from "@/../public/assets/icons/idea.svg";
+import imageFrame from "@/../public/assets/icons/ImageFrame.png";
+import magnifire from "@/../public/assets/icons/magnifire.svg";
+import roket from "@/../public/assets/icons/roket.svg";
+import stadium from "@/../public/assets/icons/stadium.svg";
 export const ICONS = {
-    fire,
-    archer,
-    idea,
-    magnifire,
-    stadium,
-    roket,
-    imageFrame
-}
+  fire,
+  archer,
+  idea,
+  magnifire,
+  stadium,
+  roket,
+  imageFrame,
+};
--- src/app/store/baseApis.ts
+++ src/app/store/baseApis.ts
@@ -1,21 +1,21 @@
-
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import Cookies from 'js-cookie';
-import { url } from '../utils/imageUtils';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Cookies from "js-cookie";
+import { url } from "../utils/imageUtils";
+// base api
 const baseApis = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl:url,
-        prepareHeaders: (headers) => {
-            const token = Cookies.get('accessToken');
-            if (token) {
-                headers.set('Authorization', `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }),
-    tagTypes: ['team'],
-    endpoints: () => ({}),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: url,
+    prepareHeaders: (headers) => {
+      const token = Cookies.get("accessToken");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["team"],
+  endpoints: () => ({}),
 });
 
 export default baseApis;
