d732ed6c1c2d33e3ba50a78c1c53e432b471df05
hosainahmeed <skyfal430@gmail.com>
2025-11-04 14:48:09 +0600

    updatre url

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["di0kc6glg2cji.cloudfront.net", "10.10.20.9", "placehold.co"],
+    domains: ["di0kc6glg2cji.cloudfront.net", "10.10.20.9", "placehold.co","api.ignitefoundation"],
   },
 };
 
--- src/app/utils/imageUtils.ts
+++ src/app/utils/imageUtils.ts
@@ -1,4 +1,4 @@
-export const url: string = "http://10.10.20.9:5070"
+export const url: string = "https://api.ignitefoundation.us"
 
 export const imageUrl = ({ image }: { image: string }) => {
     if (!image) return "https://placehold.co/400"
