1126003bbf73c5feaf67b58855db5c107864739c
hosainahmeed <skyfal430@gmail.com>
2025-11-19 18:06:05 +0600

    update

--- src/app/utils/imageUtils.ts
+++ src/app/utils/imageUtils.ts
@@ -1,5 +1,5 @@
-export const url: string = "http://10.10.20.9:5000"
-// export const url: string = "https://api.ignitefoundation.us"
+// export const url: string = "http://10.10.20.9:5000"
+export const url: string = "https://api.ignitefoundation.us"
 
 export const imageUrl = ({ image }: { image: string }) => {
     if (!image) return "https://placehold.co/400"
