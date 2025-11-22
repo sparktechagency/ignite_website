7b9e9e9bbd082c7eea41584f4150220be2c9e324
manik <maniksarker652@gmail.com>
2025-11-22 17:41:49 +0600

    s

--- src/app/font.ts
+++ src/app/font.ts
@@ -1,14 +1,14 @@
-import localFont from 'next/font/local';
-import {} from "../../public\assets\fonts\Syne-Bold.ttf"
+import localFont from "next/font/local";
+import {} from "../../publicassets\fontsSyne-Bold.ttf";
 const myCustomFont = localFont({
-    src: [
-        {
-            path: '../public/fonts/MyCustomFont-Regular.ttf', 
-            weight: '400',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-my-custom', 
+  src: [
+    {
+      path: "../public/fonts/MyCustomFont-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-my-custom",
 });
-
+//
 export default myCustomFont;
